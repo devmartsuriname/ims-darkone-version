6765d1a5257c9b35d8d7ef8fa4fecd5f3a6e532d
devmartsuriname <info@devmart.sr>
2025-09-24 16:39:31 +0530

    git commit -m "Initial commit"

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { basePath } from './context/constants.ts'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter basename={basePath}>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
